46b2abe55871665e1502e0c26ef9995d6b90f35a
Anton-lr <Anton.nydal@hotmail.com>
2022-08-27 12:24:56 +0200

    Removed error-throwing

--- src/Fetcher.js
+++ src/Fetcher.js
@@ -1,11 +1,6 @@
 async function Fetcher(data) {
     const url = data.source
     let response = await fetch(url)
-    if (!response.ok) {
-        throw new Error(
-            `This is an HTTP error: The status is ${response.status}`
-        );
-    }
     let actualData = await response.json()
     return actualData
 }
--- src/parseRequestList.js
+++ src/parseRequestList.js
@@ -6,8 +6,11 @@ async function parseRequestList(list) {
     const elements = []
     console.log(list)
     for (let i = 0; i < list.length; i++) {
+
         const data = await Fetcher(list[i]).then(b => elements.push(b))
+
     }
+
     return elements
 }
 export default parseRequestList;
