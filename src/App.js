cc19d86add97dd2381ac714b0292d2edd87f5816
Anton-lr <Anton.nydal@hotmail.com>
2022-08-28 17:18:50 +0200

    Update App.js
    Clean up from testing recursive algorithm

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useRef, useState, useEffect } from 'react';
 import Scheduler from './Scheduler';
 import parseRequestList from './parseRequestList';
-import locateNamesRec from './locateNames';
 import printVals from './printVals'
 
 function App() {
@@ -10,123 +9,6 @@ function App() {
   const [results, setResults] = useState('')
   const [isLoading, setIsLoading] = useState(false)
   const [searchDecleration, setSearchDecleration] = useState("")
-
-  const testData =
-    [
-      {
-        "type": {
-          "kode": "DAGL",
-          "beskrivelse": "Daglig leder/ adm.direktør",
-          "_links": {
-            "self": {
-              "href": "https://data.brreg.no/enhetsregisteret/api/roller/rollegruppetyper/DAGL"
-            }
-          }
-        },
-        "sistEndret": "2010-01-25",
-        "roller": [
-          {
-            "type": {
-              "kode": "DAGL",
-              "beskrivelse": "Daglig leder/ adm.direktør",
-              "_links": {
-                "self": {
-                  "href": "https://data.brreg.no/enhetsregisteret/api/roller/rolletyper/DAGL"
-                }
-              }
-            },
-            "person": {
-              "fodselsdato": "1969-08-11",
-              "navn": {
-                "fornavn": "Pål",
-                "etternavn": "Christoffersen"
-              },
-              "erDoed": false
-            },
-            "fratraadt": false,
-            "rekkefolge": 0
-          }
-        ]
-      },
-      {
-        "type": {
-          "kode": "STYR",
-          "beskrivelse": "Styre",
-          "_links": {
-            "self": {
-              "href": "https://data.brreg.no/enhetsregisteret/api/roller/rollegruppetyper/STYR"
-            }
-          }
-        },
-        "sistEndret": "2012-10-30",
-        "roller": [
-          {
-            "type": {
-              "kode": "LEDE",
-              "beskrivelse": "Styrets leder",
-              "_links": {
-                "self": {
-                  "href": "https://data.brreg.no/enhetsregisteret/api/roller/rolletyper/LEDE"
-                }
-              }
-            },
-            "person": {
-              "fodselsdato": "1969-08-11",
-              "navn": {
-                "fornavn": "Pål",
-                "etternavn": "Christoffersen"
-              },
-              "erDoed": false
-            },
-            "fratraadt": false,
-            "rekkefolge": 0
-          },
-          {
-            "type": {
-              "kode": "VARA",
-              "beskrivelse": "Varamedlem",
-              "_links": {
-                "self": {
-                  "href": "https://data.brreg.no/enhetsregisteret/api/roller/rolletyper/VARA"
-                }
-              }
-            },
-            "person": {
-              "fodselsdato": "1967-07-27",
-              "navn": {
-                "fornavn": "Runar",
-                "etternavn": "Solberg"
-              },
-              "erDoed": false
-            },
-            "fratraadt": false,
-            "rekkefolge": 1
-          }
-        ]
-      }
-    ]
-  const testData1 = [{
-    "tea": "first",
-    "coffee1": "first",
-    "coffee3": "cappuccino"
-  },
-  {
-    "water": "luke",
-    "coffee6": "americano",
-    "coffee2": "first",
-    "order1": [
-      { "water": "luke" },
-      { "tea": "second" },
-      { "coffee2": "cappuccino" }]
-  }]
-
-  const testString = 94
-  const testData3 = ["a", "b", "c"]
-  const testData4 = { "id": "2" }
-  const testData5 = [{ "a": 1, "b": 2, "c": 3 }]
-
-  console.log(JSON.stringify(locateNamesRec(testData)))
-
 
   useEffect(() => {
     if (input) {
@@ -149,8 +31,6 @@ function App() {
     setResults("")
 
   }, [input])
-
-
 
   return (
     <div className="main">
