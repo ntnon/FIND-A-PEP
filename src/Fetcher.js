63177697df2363ae549f07e590f203a2ff50b3e5
Anton-lr <Anton.nydal@hotmail.com>
2022-08-29 16:53:00 +0200

    Replaced Fetcher.js with custom hook
    useFetch is a custom react hook and has proper error handling.
    
    code written by Iamshaunjp: https://github.com/iamshaunjp/Complete-React-Tutorial/blob/lesson-20/dojo-blog/src/useFetch.js

--- src/Fetcher.js
+++
@@ -1,8 +0,0 @@
-async function Fetcher(data) {
-    const url = data.source
-    let response = await fetch(url)
-    let actualData = await response.json()
-    return actualData
-}
-
-export default Fetcher;
