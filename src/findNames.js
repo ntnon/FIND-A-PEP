5b745c663c7a458f2be1bff0cabd83c46aacaac6
Anton-lr <Anton.nydal@hotmail.com>
2022-08-29 21:44:18 +0200

    Update findNames.js

--- src/findNames.js
+++ src/findNames.js
@@ -2,12 +2,12 @@ import recSearch from "./recSearch"
 /*
 * takes a list of names. Returns only valid names
 * valid name is an object like this {"fornavn":x, "etternavn":y}
+* therefore when doing a pep search, this function will return no names
+* to accurately retrieve "name"-values from other APIs, make changes in this module
 */
 
 function findNames(r) {
-
     const listOfNames = recSearch(r, ["navn"])
-    console.log(listOfNames)
     var t = []
     for (var e in listOfNames) {
         if (listOfNames[e].fornavn) {
