c9fe224f4ca8c8dd4419b759d8c9da9b46e22da7
Anton-lr <Anton.nydal@hotmail.com>
2022-09-01 16:42:44 +0200

    The first real front end commit
    Created a grid system for the font-end to work.
    
    Structured the divs in APP to follow the grid layout. Very pleased with the aesthetic

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import getData from './Scheduler';
 
 function App() {
   const inputRef = useRef(null)
+
   const [input, setInput] = useState('')
   const [results, setResults] = useState('')
   const [isLoading, setIsLoading] = useState(false)
-  const [searchDecleration, setSearchDecleration] = useState("")
-  const [tasks, setTasks] = useState(null)
 
   useEffect(() => {
     const b = "https://code-challenge.stacc.dev/api/roller?orgNr=981078365"
@@ -19,38 +18,46 @@ function App() {
       throw Error("Request took too long to process")
     }
 
-
   }, [input]
   )
 
-
-
   return (
-    <div className="main">
-      <div className="interface">
-        <h1>
-          Welcome,
-        </h1>
-        <p>to my first API-application</p>
-        <p className="example">example searches: "981078365", "Angela Merkel"</p>
-        <input
-          ref={inputRef}
-          type="text"
-        />
-        <button
-          onClick={() => setInput(inputRef.current.value)}>
-          Confirm name
-        </button>
-      </div>
-      <div className="loading-button">
-        {isLoading ? (<p className='loadToken'>loading...</p>) : <p className='searchToken'>{searchDecleration}</p>}
-        <div className="rawResults">
-          {results}
+    <body>
+
+      <div className="grid-container">
+        <div className="header">
+          <p><a rel="stylesheet" href="https://github.com/Anton-lr/kys-stacc">GitHub page</a></p>
+          <p>My submission to <a rel="stylesheet" href="https://github.com/stacc/stacc-code-challenge-public">Stacc's code challenge</a></p>
+          <p>This project utilize <a rel="stylesheet" href="https://code-challenge.stacc.dev/">these APIs</a></p>
+
+        </div>
+        <div className="interface">
+          <div className="information">
+            <h1>SPOT-A-PEP</h1>
+            <div className="text">
+              <p>This website lets you search for the PEP-status of individuals or all individuals closely related to an organization.</p>
+              <p> When doing a company search, this website fetches data from brreg.no and performs a PEP search on the people who have important roles in that company.</p>
+            </div>
+            <div className="pepbox">
+              <p>A <i>PEP</i> is a politically Exposed Person.</p>
+              <p>Banks are required to process PEPs different from civilians.</p>
+
+            </div>
+          </div>
+          <input placeholder="Name / Organization ID"></input>
+          <btn>Search</btn>
+        </div>
+        <div className="rawdata">
+          rawdata
+        </div>
+        <div className="results">
+          results
         </div>
       </div>
-    </div>
-
+    </body >
   )
 }
+
+//interface div text, p example, input, btn
 
 export default App;
