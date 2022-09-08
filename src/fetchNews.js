90afb600fb9d09d5d75dd721614b297e5d530d91
Anton-lr <Anton.nydal@hotmail.com>
2022-09-08 11:51:07 +0200

    automatically set date

--- src/fetchNews.js
+++ src/fetchNews.js
@@ -2,7 +2,11 @@
 async function fetchNews(subject) {
     let b = subject.replaceAll(" ", " ")
     console.log(b + " retrieved")
-    return fetch("https://newsapi.org/v2/everything?q=" + b + "&domains=aftenposten.no&sortBy=popularity&from=2022-08-07&apiKey=da61097fad824208a02bd60b33e7ab9d")
+    var date = new Date();
+    var current_date = date.getFullYear() + "-" + (date.getMonth()) + "-" + (date.getDate());
+    console.log(current_date)
+    //let current_date = "2022-08-09"
+    return fetch("https://newsapi.org/v2/everything?q=" + b + "&domains=aftenposten.no&sortBy=popularity&from=" + current_date + "&apiKey=da61097fad824208a02bd60b33e7ab9d")
         .then(res => {
             return res.json()
         })
